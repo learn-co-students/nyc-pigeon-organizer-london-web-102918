2d2ed764f058dfe5a781b484fba063407a5581ca
artismarti <artismarti@gmail.com>
2018-10-24 14:47:45 +0100

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,43 +1,31 @@
 def nyc_pigeon_organizer(data)
-  pigeon_list = {}
+  pigeonpedia = {}
   data.each do |color, colors|
     colors.each do |color, color_birds|
       color_birds.each do |name|
-        pigeon_list[name] = {color: [], gender: [], lives: []}
+        pigeonpedia[name] = {color: [], gender: [], lives: []}
       end
     end
   end
   #colors
   data[:color].each do |color, color_birds|
-      color_birds.each do |name|
-        pigeon_list.keys.each do |key|
-          if key == name
-            pigeon_list[name][:color] << color.to_s
-          end
-        end
+    color_birds.each do |name|
+      pigeonpedia[name][:color] << color.to_s
     end
   end
 
  #gender
  data[:gender].each do |sex, sex_birds|
    sex_birds.each do |name|
-     pigeon_list.keys.each do |key|
-       if key == name
-         pigeon_list[name][:gender] << sex.to_s
-       end
-     end
+    pigeonpedia[name][:gender] << sex.to_s
    end
  end
 
  #lives
  data[:lives].each do |location, location_birds|
    location_birds.each do |name|
-     pigeon_list.keys.each do |key|
-       if key == name
-         pigeon_list[name][:lives] << location.to_s
-       end
-     end
+     pigeonpedia[name][:lives] << location.to_s
    end
  end
-  pigeon_list
+  pigeonpedia
 end
