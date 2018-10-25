7201de05487c566e929e49ac3208a7dabfd9191a
sim.catley <sim.catley@hotmail.com>
2018-10-25 09:07:14 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |description, options|
+    options.each do |option, pigeons|
+      pigeons.each do |pigeon|
+        new_hash[pigeon] ||= {}
+        new_hash[pigeon][description] ||= []
+        new_hash[pigeon][description] << option.to_s
+      end
+    end
+  end
+  new_hash
 end
