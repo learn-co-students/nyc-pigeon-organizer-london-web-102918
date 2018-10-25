b9b609aa5a4a4a6574143d24e95b6e45c09ddf2c
Paul-Cl-ark <clark.paul123@gmail.com>
2018-10-25 13:21:36 +0100

    Done

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |attribute, types|
+    types.each do |type, pigeon_array|
+      pigeon_array.each do |pigeon|
+        pigeon_list[pigeon] ||= {}
+        pigeon_list[pigeon][attribute] ||= []
+        pigeon_list[pigeon][attribute].push(type.to_s)
+      end
+    end
+  end
+  pigeon_list
 end
+
+# double pipe equals "will only complete the assignment if
+# the left side of our operation returns false or nil."
+# https://medium.com/@jaredrayjohnson1/ruby-operators-double-pipe-equals-bfcbe21a7177
